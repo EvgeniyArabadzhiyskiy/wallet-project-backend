5832d6df338a0aa3faddfaf66fc9be60f60477f6
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-19 16:46:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(express.json());
 
 app.use("/api/users", authRouter);
