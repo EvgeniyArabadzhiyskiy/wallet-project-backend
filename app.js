5bfe71744e27d0ff8421556925532fadcd87a95b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-19 18:22:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use("/api/transactions", transactionsRouter);
 // app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found 7777" });
+  res.status(404).json({ message: "Not found 888" });
 });
 
 app.use((err, req, res, next) => {
