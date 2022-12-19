fe59512b7bd0b975dd689b958eee8eaf17fa1bbf
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-19 18:05:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerDocument = require("./swagger.json");
 
 const { authRouter, transactionsRouter } = require("./routes/api");
 
@@ -15,17 +15,25 @@ app.use(logger(formatsLogger));
 app.use(express.json());
 app.use(cors());
 
+app.get("/djon", (req, res) => {
+  res.json({ result: "GET Vercel", status: "DJON success" });
+});
+
+app.post("/poly", (req, res) => {
+  res.json({ result: "POST Vercel", status: "POLY success" });
+});
+
 app.use("/api/users", authRouter);
 app.use("/api/transactions", transactionsRouter);
-app.use("/api-docs", swaggerUi.serve);
-app.get("/api-docs", swaggerUi.setup(swaggerDocument));
+// app.use("/api-docs", swaggerUi.serve);
+// app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
-	res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-	res.status(500).json({ message: err.message });
+  res.status(500).json({ message: err.message });
 });
 
 module.exports = app;
