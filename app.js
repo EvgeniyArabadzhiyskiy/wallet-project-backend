234b2c46b4f9ffd7d9a970515f5a1429cf637e17
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-19 20:37:50 +0200

    commit

--- app.js
+++ app.js
@@ -2,33 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./swagger.json");
-
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-// module.exports = allowCors(handler)
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const { authRouter, transactionsRouter } = require("./routes/api");
 
@@ -40,20 +15,19 @@ app.use(logger(formatsLogger));
 app.use(express.json());
 app.use(cors());
 
+// app.get("/djon", (req, res) => {
+//   res.json({ result: "GET Vercel", status: "DJON success" });
+// });
 
-app.get("/djon",  (req, res) => {
-  res.json({ result: "GET Vercel", status: "DJON success" });
-});
-
-app.post("/poly",  (req, res) => {
-  const body = req.body
-  res.json({ result: "POST Vercel", status: "POLY success", body });
-});
+// app.post("/poly", (req, res) => {
+//   const body = req.body;
+//   res.json({ result: "POST Vercel", status: "POLY success", body });
+// });
 
 app.use("/api/users", authRouter);
 app.use("/api/transactions", transactionsRouter);
-// app.use("/api-docs", swaggerUi.serve);
-// app.get("/api-docs", swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found 777" });
@@ -62,7 +36,5 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
-
-allowCors(handler)
 
 module.exports = app;
