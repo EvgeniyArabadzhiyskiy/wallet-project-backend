cdea1f97600b856fd746fedba0c5304c83eb4b4f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-21 16:57:43 +0200

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const authenticate = require("./authenticate");
-const isValidId = require('./isValidId');
-const validateBody = require('./validateBody');
+const isValidId = require("./isValidId");
+const validateBody = require("./validateBody");
+const controllerWrapper = require("./controllerWrapper");
 
 module.exports = {
-    authenticate,
-    isValidId,
-    validateBody
+  authenticate,
+  isValidId,
+  validateBody,
+  controllerWrapper,
 };
