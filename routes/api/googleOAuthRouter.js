2a65a8a7497d57ef24e232eac6a2efcdef8fc4fe
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-22 16:37:12 +0200

    Update googleOAuthRouter.js

--- routes/api/googleOAuthRouter.js
+++ routes/api/googleOAuthRouter.js
@@ -1,86 +1,100 @@
-const queryString = require('query-string');
-const axios = require('axios');
+const queryString = require("query-string");
+const axios = require("axios");
 require("dotenv").config();
+const bcrypt = require("bcryptjs");
 const express = require("express");
 
 const router = express.Router();
-// const { User } = require('../db/userModel');
-const { controllerWrapper } = require('../../middlewares');
+const { User } = require("../../models/user");
+const { controllerWrapper } = require("../../middlewares");
+const createToken = require("../../helpers/createToken");
 
-const {BASE_URL} = process.env
-const {FRONTEND_URL} = process.env
-const {GOOGLE_CLIENT_ID} = process.env
-const {GOOGLE_CLIENT_SECRET} = process.env
-
+const { BASE_URL } = process.env;
+const { FRONTEND_URL } = process.env;
+const { GOOGLE_CLIENT_ID } = process.env;
+const { GOOGLE_CLIENT_SECRET } = process.env;
 
 const googleAuth = async (req, res) => {
-    const stringifiedParams = queryString.stringify({
-        client_id: GOOGLE_CLIENT_ID,
-        redirect_uri: `${BASE_URL}/auth-google/google-redirect`,
-        scope:[
-            'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/userinfo.profile',
-        ].join(" "),
-        response_type: 'code',
-        access_type: 'offline',
-        prompt: 'consent',
-        
-    })
-    
-    return res.redirect(
-        `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
-    )
-}
+  const stringifiedParams = queryString.stringify({
+    client_id: GOOGLE_CLIENT_ID,
+    redirect_uri: `${BASE_URL}/auth-google/google-redirect`,
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" "),
+    response_type: "code",
+    access_type: "offline",
+    prompt: "consent",
+  });
+
+  return res.redirect(
+    `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
+  );
+};
 
 const googleRedirect = async (req, res) => {
-   const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
 
-   const urlOdject = new URL(fullUrl);
-   const urlParams = queryString.parse(urlOdject.search);
-   const code = urlParams.code;
+  const urlOdject = new URL(fullUrl);
+  const urlParams = queryString.parse(urlOdject.search);
+  const code = urlParams.code;
 
-   const tokenData = await axios({
-    url: 'https://oauth2.googleapis.com/token',
-    method: 'POST',
+  const tokenData = await axios({
+    url: "https://oauth2.googleapis.com/token",
+    method: "POST",
     data: {
-        client_id: GOOGLE_CLIENT_ID,
-        client_secret: GOOGLE_CLIENT_SECRET,
-        redirect_uri: `${BASE_URL}/auth-google/google-redirect`,
-        grant_type: 'authorization_code',
-        code: code,
-    }
-   });
+      client_id: GOOGLE_CLIENT_ID,
+      client_secret: GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${BASE_URL}/auth-google/google-redirect`,
+      grant_type: "authorization_code",
+      code: code,
+    },
+  });
 
-   const userData = await axios({
-    url: 'https://www.googleapis.com/oauth2/v2/userinfo',
-    method: 'GET',
+  const userData = await axios({
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    method: "GET",
     headers: {
-       'Authorization': `Bearer ${tokenData.data.access_token}`
-    }
-   })
-    // console.log("googleRedirect  userData", userData.data);
+      Authorization: `Bearer ${tokenData.data.access_token}`,
+    },
+  });
 
-    const email = userData.data.email
-    // console.log("googleRedirect  email", email);
-    // console.log("googleRedirect  userData", userData.data.email);
+  const { id: googleId, email, name } = userData.data;
+  const user = await User.findOne({ email });
+  
+  if (!user) {
+    const hashPassword = await bcrypt.hash(googleId, 10);
 
-    // const user = await User.find({email});
-    // console.log("googleRedirect  user", user);
-    
-   
-    // return res.redirect(
-    //     `${FRONTEND_URL}?accessToken=${tokenData.data.access_token}`
-    // )
+    const googleUser = await User.create({
+      email,
+      password: hashPassword,
+      firstName: name,
+      balance: 0,
+    });
+
+    console.log("googleUser", googleUser);
+
+    const token = createToken({ id: googleUser._id });
+    await User.findByIdAndUpdate( googleUser._id, { token });
 
     return res.redirect(
-        `${FRONTEND_URL}?accessToken=${email}`
-    )
-}
+      `${FRONTEND_URL}?accessToken=${token}`
+    );
+  }
+
+  const token = createToken({ id: user._id });
+  await User.findByIdAndUpdate( user._id, { token });
+
+  return res.redirect(
+    `${FRONTEND_URL}?accessToken=${token}`
+  );
+
+};
 
 router.get("/google", googleAuth);
 router.get("/google-redirect", controllerWrapper(googleRedirect));
 
-module.exports = router;  
+module.exports = router;
 
 // const queryString = require('query-string');
 // const axios = require('axios');
@@ -96,7 +110,6 @@ module.exports = router;
 // // const {GOOGLE_CLIENT_ID} = process.env
 // // const {GOOGLE_CLIENT_SECRET} = process.env
 
-
 // const googleAuth = async (req, res) => {
 //     const stringifiedParams = queryString.stringify({
 //         client_id: '60890143461-jtiqjs6s9kvffqal70e6ecmtjb9gqh2e.apps.googleusercontent.com',
@@ -108,14 +121,14 @@ module.exports = router;
 //         response_type: 'code',
 //         access_type: 'offline',
 //         prompt: 'consent',
-        
+
 //     })
 //     console.log("googleAuth  stringifiedParams", stringifiedParams);
 //     // console.log("hello");
-    
+
 //     return res.redirect(
 //         `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
-        
+
 //     )
 // }
 
@@ -153,8 +166,7 @@ module.exports = router;
 
 //     // const user = await User.find({email});
 //     // console.log("googleRedirect  user", user);
-    
-   
+
 //     // return res.redirect(
 //     //     `${FRONTEND_URL}?accessToken=${tokenData.data.access_token}`
 //     // )
@@ -167,4 +179,4 @@ module.exports = router;
 // router.get("/google", googleAuth);
 // router.get("/google-redirect", controllerWrapper(googleRedirect));
 
-// module.exports = router;  
+// module.exports = router;
