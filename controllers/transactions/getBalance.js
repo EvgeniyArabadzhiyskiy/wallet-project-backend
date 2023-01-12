455ac81172363917cf3177f8804da575f9d9f3ab
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-12 18:53:58 +0200

    Create getBalance.js

---
+++ controllers/transactions/getBalance.js
@@ -0,0 +1,15 @@
+const { User } = require("../../models/user");
+
+const getBalance = async (req, res, next) => {
+  try {
+    const id = req.userId;
+    const user = await User.findById(id);
+    const userBalance = user ? user.balance : null;
+
+    res.status(200).json({userBalance});
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getBalance;
