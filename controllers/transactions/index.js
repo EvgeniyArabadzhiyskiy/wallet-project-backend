f22d1aec0fd58106b0927dd9a996107143eb3099
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-12 18:54:02 +0200

    Update index.js

--- controllers/transactions/index.js
+++ controllers/transactions/index.js
@@ -2,5 +2,12 @@ const add = require("./add");
 const getAllTransactions = require("./getAllTransactions");
 const getStatistics = require("./getStatistics");
 const getCategories = require("./getCategories");
+const getBalance = require("./getBalance");
 
-module.exports = { add, getAllTransactions, getStatistics, getCategories };
+module.exports = {
+  add,
+  getAllTransactions,
+  getStatistics,
+  getCategories,
+  getBalance,
+};
