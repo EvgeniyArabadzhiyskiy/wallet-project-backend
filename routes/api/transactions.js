1da90ef99f057f7d5a7416388cb2b9ba3dcdf096
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-22 15:07:06 +0200

    Update transactions.js

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post("/", authenticate, ctrl.add);
 router.get("/", authenticate, ctrl.getAllTransactions);
+router.delete("/:transactionId", authenticate, ctrl.deleteTransaction);
 router.get("/balance", authenticate, ctrl.getBalance);
 router.get("/statistics", authenticate, ctrl.getStatistics);
 router.get("/categories", authenticate, ctrl.getCategories);
