ee9b46f900b5690e15d253cc9b84373d2dbdb2d5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-27 11:44:29 +0200

    Create editTransaction.js

---
+++ controllers/transactions/editTransaction.js
@@ -0,0 +1,39 @@
+const { User } = require("../../models/user");
+const { Transaction, addSchema } = require("../../models/transaction");
+const { RequestError } = require("../../utils");
+
+const editTransaction = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const { transactionId } = req.params;
+    const { error } = addSchema.validate(req.body);
+    if (error) throw new Error(error);
+    const timestamps = Number(new Date(req.body.date).getTime());
+
+    const prevResult = await Transaction.findOne({ _id: transactionId, owner: userId });
+    
+    const { typeOperation: prevTypeOperation, amount: prevAmount } = prevResult;
+    const prevSum = prevTypeOperation === "income" ? prevAmount : -prevAmount;
+
+    const result = await Transaction.findOneAndUpdate(
+      { _id: transactionId, owner: userId }, { ...req.body, timestamps}, { new: true }
+    );
+
+    const { typeOperation, amount } = result;
+    const sum = typeOperation === "income" ? amount : -amount;
+
+    const user = await User.findById({ _id: userId });
+    if (!user) throw RequestError(404);
+
+    const newSum = prevSum - sum;
+
+    const newBalance = user.balance - newSum;
+    await User.findByIdAndUpdate({ _id: userId }, { balance: newBalance });
+
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = editTransaction;
