58b989f96f36418d0da6da587898f84150be3c80
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-08 22:24:19 +0200

    upd middleware

--- middlewares/controllerWrapper.js
+++ middlewares/controllerWrapper.js
@@ -1,7 +1,6 @@
 const controllerWrapper = (ctrl) => {
   return (req, res, next) => {
     ctrl(req, res).catch((err) => {
-      // console.log("return ~ err", err.message);
       next(err);
     });
   };
