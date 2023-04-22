a38b80409930e2c060516ce86beda2d228391777
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-04-22 18:07:37 +0300

    Update add.js

--- controllers/transactions/add.js
+++ controllers/transactions/add.js
@@ -31,25 +31,25 @@ const add = async (req, res, next) => {
 			balanceAfterTransaction: balance,
 		});
 		
-		const allTransactions = await Transaction.find(
-			{ owner: _id },
-			{ _id: 1 }
-		  ).sort({ timestamps: -1, createdAt: -1 });
-		   // Так не работает Будет ошибка
-		   //   .findIndex((element) => element._id.toString() === transaction._id.toString())
+		// const allTransactions = await Transaction.find(
+		// 	{ owner: _id },
+		// 	{ _id: 1 }
+		//   ).sort({ timestamps: -1, createdAt: -1 });
+		//    // Так не работает Будет ошибка
+		//    //   .findIndex((element) => element._id.toString() === transaction._id.toString())
 		
-        // Если не сделать копию allTransactions то получаем ошибку
-		  const indexNewTransaction = allTransactions.findIndex(     
-			(element) => element._id.toString() === transaction._id.toString()
-		  );
+        // // Если не сделать копию allTransactions то получаем ошибку
+		//   const indexNewTransaction = allTransactions.findIndex(     
+		// 	(element) => element._id.toString() === transaction._id.toString()
+		//   );
 
 		
-		const result = {
-			...transaction._doc,  // Если вернуть просто transaction то почемуто приходит объект с системными данными
-			position: indexNewTransaction,
-		};
-		
-		res.status(201).json(result);
+		// const result = {
+		// 	...transaction._doc,  // Если вернуть просто transaction то почемуто приходит объект с системными данными
+		// 	position: indexNewTransaction,
+		// };
+
+		res.status(201).json(transaction);
 	} catch (error) {
 		next(error);
 	}
