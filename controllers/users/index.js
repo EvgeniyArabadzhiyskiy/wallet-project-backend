a22b201508eac0f55f34ed2eaa691d70e8930c5f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-01 21:55:32 +0300

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const register = require("./register");
 const login = require("./login");
 const logOut = require("./logOut");
 const current = require("./current");
+const test = require("./test");
 
 module.exports = {
   register,
   login,
   logOut,
   current,
+  test,
 };
