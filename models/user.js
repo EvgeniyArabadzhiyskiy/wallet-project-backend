ff536ea1ee9ad0d436ac0d2eef44532126da4b02
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-06 21:29:13 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,56 +6,58 @@ const { handleSaveErrors } = require("../utils");
 // const emailRegexp = /^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
 
 const userSchema = new Schema(
-	{
-		email: {
-			type: String,
-			required: [true, "Email is required"],
-			// match: [emailRegexp, "Email is invalid"],
-			unique: true,
-		},
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      // match: [emailRegexp, "Email is invalid"],
+      unique: true,
+    },
 
-		password: {
-			type: String,
-			minLenght: [6, "Passwords must be at least 6 characters long."],
-			required: [true, "Password is required"],
-		},
+    password: {
+      type: String,
+      minLenght: [6, "Passwords must be at least 6 characters long."],
+      required: [true, "Password is required"],
+    },
 
-		firstName: {
-			type: String,
-			minLenght: [2, "First Name must be at least 2 characters long."],
-		},
+    firstName: {
+      type: String,
+      minLenght: [2, "First Name must be at least 2 characters long."],
+    },
 
-		token: {
-			type: String,
-			default: null,
-		},
-		
-		balance: Number,
-		default: 0,
-	},
-	{ versionKey: false, timestamps: true }
+    token: {
+      type: String,
+      default: null,
+    },
+
+    balance: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { versionKey: false, timestamps: true }
 );
 
 userSchema.post("save", handleSaveErrors);
 
 const sigUpInSchema = Joi.object({
-	email: Joi.string()
-		// .pattern(emailRegexp)
-		.required(),
-	password: Joi.string().min(6).required(),
-	firstName: Joi.string().min(2).required(),
+  email: Joi.string()
+    // .pattern(emailRegexp)
+    .required(),
+  password: Joi.string().min(6).required(),
+  firstName: Joi.string().min(2).required(),
 });
 
 const logInShema = Joi.object({
-	email: Joi.string()
-		// .pattern(emailRegexp)
-		.required(),
-	password: Joi.string().min(6).required(),
+  email: Joi.string()
+    // .pattern(emailRegexp)
+    .required(),
+  password: Joi.string().min(6).required(),
 });
 
 const schemas = {
-	register: sigUpInSchema,
-	logIn: logInShema,
+  register: sigUpInSchema,
+  logIn: logInShema,
 };
 
 const User = model("users", userSchema);
